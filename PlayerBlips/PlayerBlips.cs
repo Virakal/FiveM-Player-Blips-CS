a8ce0bdf8ce6a45a7489580ad64153eca596f298
Jon Goodger <jonno.is@gmail.com>
2018-01-06 05:53:01 +0000

    Add project files.

---
+++ PlayerBlips/PlayerBlips.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Virakal.FiveM.PlayerBlips
+{
+    public class PlayerBlips : BaseScript
+    {
+        private IDictionary<Player, Blip> blips { get; } = new Dictionary<Player, Blip>(PlayerList.MaxPlayers);
+
+        public PlayerBlips()
+        {
+            Tick += OnTick;
+        }
+
+        private async Task OnTick()
+        {
+            var playerList = new PlayerList();
+
+            foreach (var player in playerList)
+            {
+
+            }
+
+            await Delay(100);
+        }
+    }
+}
